8e355bc2cd294bf958e22551d1265b4f2fe5651f
Cem Gulec <cem.ggulecc@gmail.com>
2019-11-08 18:25:07 +0300

    set for bst configuration

---
+++ cmake-build-debug/CMakeFiles/Binary_Search_Trees.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Binary_Search_Trees.dir/main.c.obj"
+  "Binary_Search_Trees.pdb"
+  "Binary_Search_Trees.exe"
+  "Binary_Search_Trees.exe.manifest"
+  "libBinary_Search_Trees.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Binary_Search_Trees.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,291 @@
+/*Designed and programmed by - Cem Gulec - 150117828*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct bst_tree {
+    int data;
+    struct bst_tree *left, *right;
+    int depthLevel;
+};
+typedef struct bst_tree node;
+
+node * root = NULL;
+int numArray[20], i=0, num, decision;
+FILE *fptr;
+
+/*prototypes*/
+node * insert(node * tree, int val);
+node * delete(node * tree, int val);
+void print_lnr(node * tree);
+node * getMin(node *tree);
+node * getMax(node *tree);
+node * tracePath(node *tree, int val);
+void printWithCondition(node *tree, int val);
+int checkTerm(int val);
+int checkDepth(node *tree, int val, int level);
+void deleteByTheUserEntered();
+void tracePathByTheUserEntered();
+
+void main()
+{
+    //read file from the file
+    fptr = fopen("C:/Users/cemgg/Documents/GitHub/Binary-Search-Trees/input.txt","r");
+
+    //if file does not exist
+    if(fptr == NULL){
+        printf("Couldn't open the file");
+        exit(-1);
+    }
+    int data=0;
+
+    //initializing a temporary node
+    node *tmp;
+
+    /* Inserting nodes into tree */
+
+    for(; fscanf(fptr, "%d", &data) && !feof(fptr);)
+    {
+        root = insert(root,data);
+    }
+
+    root = insert(root,data); //inserting last element in the text file
+
+    /* Printing nodes of tree in LMNR fashion */
+    printf("lmnr display\n");
+    print_lnr(root);
+
+    printf("Press (1) to delete a node\nPress(2) to trace path of a nodepress\n(-1) to exit\nDecision: ");
+
+    scanf("%d",&decision);
+
+    while(decision != -1){
+        switch (decision){
+            case 1:
+                deleteByTheUserEntered();
+                break;
+            case 2:
+                tracePathByTheUserEntered();
+                break;
+            case -1:
+                exit(-1);
+        }
+
+        printf("Press (1) to delete a node\nPress(2) to trace path of a nodepress\n(-1) to exit\nDecision: ");
+        scanf("%d",&decision);
+
+    }
+
+
+
+    fclose(fptr);
+}
+
+//inserting a new node into tree
+node * insert(node * tree, int val){
+    //creating a node in order to place the value
+    if( tree == NULL ){
+        node *tmp = (node *)malloc(sizeof(node));
+        tmp->data = val;
+        numArray[i++]=val;
+        tmp->left = tmp->right = NULL;
+        tree = tmp;
+        tmp = NULL;
+    }
+        //looking for the right place to put node, iterating through its left, middle and right branches
+    else if( val < tree->data ){
+        tree->left = insert(tree->left,val);
+    }
+    else if( val >= tree->data ){
+            tree->right =  insert(tree->right,val);
+
+
+    }else{}
+
+    return tree;
+
+}
+
+//deleting a node
+node * delete(node * tree, int val){
+
+    // base case
+    if (tree == NULL) return tree;
+
+    // If the key to be deleted is smaller than the root's key,
+    // then it lies in left subtree
+    if (val < tree->data)
+        tree->left = delete(tree->left, val);
+
+        // If the key to be deleted is greater than the root's key,
+        // then it lies in right subtree
+    else if (val > tree->data)
+        tree->right = delete(tree->right, val);
+
+        // if key is same as root's key, then This is the node
+        // to be deleted
+    else
+    {
+        // node with only one child or no child
+        if (tree->left == NULL)
+        {
+            node *temp = tree->right;
+            free(tree);
+            return temp;
+        }
+        else if (tree->right == NULL)
+        {
+            node *temp = tree->left;
+            free(tree);
+            return temp;
+        }
+
+        // node with two children: Get the inorder successor (smallest
+        // in the right subtree)
+        node* temp = getMin(tree->right);
+
+        // Copy the inorder successor's content to this node
+        tree->data = temp->data;
+
+        // Delete the inorder successor
+        tree->right = delete(tree->right, temp->data);
+    }
+    return tree;
+}
+
+//print all the nodes in LNR fashion
+void print_lnr(node * tree){
+    if (tree)
+    {
+        print_lnr(tree->left);
+        printf("%d\n",tree->data);
+        print_lnr(tree->right);
+
+    }
+
+}
+
+//get left most node by iterating recursively
+node * getMin(node *tree){
+    node *currentNode = tree;
+    while (currentNode->left != NULL)
+        currentNode = currentNode->left;
+
+    return currentNode;
+}
+
+//get right most node by iterating recursively
+node * getMax(node *tree){
+    node *currentNode = tree;
+    while (currentNode->right != NULL)
+        currentNode = currentNode->right;
+
+    return currentNode;
+}
+
+//trace path of the wanted node, same way as declared in insert function
+node * tracePath(node *tree, int val){
+
+    //print the root
+    printWithCondition(tree,val);
+    //if there exists nodes
+    while(tree!=NULL){
+        //if not found yet
+        if(tree->data!=val){
+            //look through its left branch and print
+            if( val < tree->data ){
+                tree->left = tracePath(tree->left,val);
+                printWithCondition(tree,val);
+            }
+            else if( val >= tree->data ){
+                //look through its right branch and print
+                    tree->right =  tracePath(tree->right,val);
+                    printWithCondition(tree,val);
+
+
+            }else{}
+            return tree;
+        }
+
+    }
+}
+
+//in order to delete last "->" symbol, defined following conditions
+void printWithCondition(node *tree, int val){
+
+    //if we reached the data on the path, no more "->" is needed
+    if(tree->data == val){
+        printf("%d", tree->data);
+
+    }
+    else{
+        printf("%d -> ",tree->data);
+    }
+
+}
+
+//this function checks if the term is exist
+int checkTerm(int val){
+
+    int i;
+
+    for(i=0; i<20; i++){
+        if(numArray[i] == val)
+            return 1;
+    }
+
+    return 0;
+}
+
+//return the level of wanted node
+int checkDepth(node *tree, int val, int depthLevel){
+
+    //if not a single node exists
+    if(tree == NULL)
+        return 0;
+
+    //if found the node
+    if(tree->data == val)
+        return depthLevel;
+
+    //if not found recursively look the other branches
+    return checkDepth(tree->left, val, depthLevel+1)
+           | checkDepth(tree->right, val, depthLevel+1);
+
+}
+
+void deleteByTheUserEntered(){
+    //get the value which is wanted to be deleted
+    printf("write the number to delete: ");
+    scanf("%d",&num);
+
+    //if it does exist
+    if(checkTerm(num) == 1)
+    {
+        //assign the last form into root
+        root = delete(root, num);
+        printf("\ndeleted\n**re-construct tree by the new formation**\nlnr display\n");
+
+        print_lnr(root);
+    }
+        //if does not exist
+    else
+        printf("does not exist\n\n");
+}
+
+void tracePathByTheUserEntered(){
+    //select the node to trace path
+    printf("write the number to trace path: ");
+    scanf("%d",&num);
+
+    //if selected value does exist, look through its path and depth information
+    if(checkTerm(num) == 1)
+    {
+        printf("found in the depth level: %d",checkDepth(root,num,1));
+        printf("\nand traced with the following path:  ");
+        tracePath(root,num);
+    }
+    else
+        printf("does not exist\n\n");
+}
