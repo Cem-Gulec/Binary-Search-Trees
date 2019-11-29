91bc83dca93a8ee73e51f539161641cf1b55eb21
Cem Gulec <cem.ggulecc@gmail.com>
2019-11-29 22:36:28 +0300

    sequence calculated | unsigned long long

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*Designed and programmed by - Cem Gulec - 150117828
  *
- * Constructed tree:
+ * An example constructed tree:
  *                18
  *         /              \
  *       12                36
@@ -12,12 +12,12 @@
  * 2       15            21
  *
  * TODO: output düzenlenecek
- * TODO: assign order kısmında tüm nodeları arrayde tut iterative şekilde tüm nodeları trace et ve atama yap
  * TODO: kodlar biraz daha düzenlenebilir
  * TODO: yorumlama daha fazla eklenebilir
  * TODO: main içi düzenle
  * TODO: tracepath için bir seçenek sun
  * TODO: 180bin sequence'in printlenmesi
+ * TODO: data rapor
  * */
 
 #include <stdlib.h>
@@ -31,14 +31,14 @@ struct bst_tree {
     int right_size;
     int total_size;
     int depth;
-    int topology_num;
-    int ord;
+    unsigned long long int topology_num;
+    unsigned long long int ord;
     struct bst_tree *left, *right;
 };
 typedef struct bst_tree node;
 
 node * root = NULL;
-int numArray[20], i=0, num, decision;
+int numArray[50], i=0, num, decision;
 FILE *fptr;
 
 /*prototypes*/
@@ -49,56 +49,73 @@ node * getMin(node *);
 node * getMax(node *);
 int getMaxDepth(node *);
 int getSize(node *);
-int getDifTopology(int);
+unsigned long long int getDifTopology(int);
 int calculateCatalan(int);
 int binomialCoeF(int,int);
-unsigned int factorial(unsigned int);
+unsigned long long int factorial(unsigned int);
 node * traceNode (node *, int );
 int checkTerm(int);
 int checkDepth(node *, int, int);
+void checkDepthByTheUserEntered();
+void getSizeByTheUserEntered();
+void insertByTheUserEntered();
 void deleteByTheUserEntered();
 void tracePathByTheUserEntered();
 void splitString(char []);
 void assignDepths(node *);
 void assignSize(node *);
 void assignSizeLR(node *);
-void assignTopology(node *);
 void assignOrd(node *);
 
 void main()
 {
+    int insert_number;
     char str[200];
     printf("Enter your sequence of numbers: ");
     gets(str);
+    //while splitting the sequence of numbers given
+    //it also adds elements to the tree
     splitString(str);
 
-    /* Printing nodes of tree in LNR fashion */
     assignDepths(root);
     assignSize(root);
     assignSizeLR(root);
-    assignTopology(root);
     assignOrd(root);
+
+    /* Printing nodes of tree in LNR fashion */
     printf("LNR display\n");
     print_lnr(root);
-    printf("\nmax depth level: %d\n",getMaxDepth(root));
-    printf("\nsize of the root: %d\n",getSize(traceNode(root,36)));
-    printf("\ndepth level: %d\n",checkDepth(root,36,1));
-    printf("\nPress (1) to delete a node\nPress(2) to trace path of a nodepress\n(-1) to exit\nDecision: ");
+    printf("\nMax depth level: %d\n",getMaxDepth(root));
+    printf("Min element of the tree: %d  |  Max element of the tree: %d\n", getMin(root)->data,getMax(root)->data);
+    printf("Size of the root: %d  |  # of_left_nodes: %d  |  # of_right_nodes: %d\n",root->total_size, root->left_size, root->right_size);
+    printf("\nPress (1) to insert a node\nPress (2) to trace path of a node\n"
+           "Press (3) to get size of a node\nPress (4) check depth of a node\n      (-1) to exit\nDecision: ");
     scanf("%d",&decision);
 
     while(decision != -1){
         switch (decision){
+            //insert another node
             case 1:
-                deleteByTheUserEntered();
+                insertByTheUserEntered();
                 break;
+            //trace path of a node
             case 2:
                 tracePathByTheUserEntered();
                 break;
+            //track size information of a node
+            case 3:
+                getSizeByTheUserEntered();
+                break;
+            case 4:
+                checkDepthByTheUserEntered();
+                break;
+            //press -1 to exit loop else continue asking
             case -1:
                 exit(-1);
         }
 
-        printf("\nPress (1) to delete a node\nPress(2) to trace path of a nodepress\n(-1) to exit\nDecision: ");
+        printf("\nPress (1) to insert a node\nPress (2) to trace path of a node\n"
+               "Press (3) to get size of a node\nPress (4) check depth of a node\n      (-1) to exit\nDecision: ");
         scanf("%d",&decision);
 
     }
@@ -178,8 +195,8 @@ node * delete(node * tree, int val){
 void print_lnr(node * tree){
     if (tree){
         print_lnr(tree->left);
-        printf("%d - d_l:%d - #left:%d - #right:%d - #total_size:%d - #topology:%d - ord:%d\n",tree->data, tree->depth,
-                tree->left_size, tree->right_size, tree->total_size, tree->topology_num, tree->ord);
+        printf("%d -> d_l:%d - #left:%d - #right:%d - #total_size:%d - ord:%llu\n",tree->data, tree->depth,
+                tree->left_size, tree->right_size, tree->total_size, tree->ord);
         print_lnr(tree->right);
     }
 }
@@ -230,8 +247,8 @@ int getSize(node * tree){
 }
 
 //a function to calculate number of topologies of a given node
-int getDifTopology(int node_size){
-    int count = calculateCatalan(node_size);
+unsigned long long int getDifTopology(int node_size){
+    unsigned long long int count = calculateCatalan(node_size);
 
     return count;
 }
@@ -260,7 +277,7 @@ int binomialCoeF(int node_size, int k){
     return res;
 }
 
-unsigned int factorial(unsigned int n){
+unsigned long long int factorial(unsigned int n){
     if (n == 0)
         return 1;
     return n * factorial(n - 1);
@@ -278,10 +295,11 @@ node * traceNode (node *tree, int val){
 }
 
 //this function checks if the term is exist
+//if it exists return 1
 int checkTerm(int val){
     int i;
 
-    for(i=0; i<20; i++){
+    for(i=0; i<50; i++){
         if(numArray[i] == val)
             return 1;
     }
@@ -303,33 +321,63 @@ int checkDepth(node *tree, int val, int depthLevel){
            | checkDepth(tree->right, val, depthLevel+1);
 }
 
-void deleteByTheUserEntered(){
-    //get the value which is wanted to be deleted
-    printf("write the number to delete: ");
-    scanf("%d",&num);
+void checkDepthByTheUserEntered(){
+    int number,depth;
+    printf("Which node's depth information: ");
+    scanf("%d",&number);
 
-    //if it does exist
-    if(checkTerm(num) == 1){
-        //assign the last form into root
-        root = delete(root, num);
-        printf("\ndeleted\n**re-construct tree by the new formation**\nlnr display\n");
-        print_lnr(root);
+    if(checkTerm(number) == 1){
+        depth = checkDepth(root,number,1);
+        printf("\n\t\t\t**Depth level of \"%d\": %d\n",number,depth);
     }
-    //if does not exist
     else
-        printf("does not exist\n\n");
+        printf("\n\t\t\t**It doesn't exist in the tree**\n\t\t\tReturning back to selection panel\n\n");
+}
+
+void getSizeByTheUserEntered(){
+    int number,size;
+    printf("Which node's size information: ");
+    scanf("%d",&number);
+
+    if(checkTerm(number) == 1){
+        size = getSize(traceNode(root,number));
+        printf("\n\t\t\tSize of the root: %d | # of_left_nodes: %d | # of_right_nodes: %d\n\n",size, traceNode(root,number)->left_size, traceNode(root,number)->right_size);
+    }
+    else
+        printf("\n\t\t\t**It doesn't exist in the tree**\n\t\t\tReturning back to selection panel\n\n");
+}
+
+void insertByTheUserEntered(){
+    int insertion_number;
+    printf("Number to be inserted: ");
+    scanf("%d",&insertion_number);
+
+    if(checkTerm(insertion_number) == 0){
+        root = insert(root,insertion_number);
+        assignDepths(root);
+        assignSize(root);
+        assignSizeLR(root);
+        assignOrd(root);
+        printf("\n\t\t\t**Number has been inserted**\n\t\t\tRe-constructing tree by the new formation\n\t\t\tLNR display\n\n");
+        print_lnr(root);
+        printf("\nMax depth level: %d\n",getMaxDepth(root));
+        printf("Min element of the tree: %d  |  Max element of the tree: %d\n", getMin(root)->data,getMax(root)->data);
+        printf("Size of the root: %d  |  # of_left_nodes: %d  |  # of_right_nodes: %d\n",root->total_size, root->left_size, root->right_size);
+    }
+    else
+        printf("\n\t\t\t**It already exists in tree**\n\t\t\tReturning back to selection panel\n\n");
 }
 
 void tracePathByTheUserEntered(){
     //select the node to trace path
-    printf("write the number to trace path: ");
+    printf("Write number to be traced: ");
     scanf("%d",&num);
 
     //if selected value does exist, look through its path and depth information
     if(checkTerm(num) == 1)
-        printf("found in the depth level: %d",checkDepth(root,num,1));
+        printf("Found in the depth level: %d",checkDepth(root,num,1));
     else
-        printf("does not exist\n\n");
+        printf("\n\t\t\t**The number does not exist\n\t\t\tReturning back to selection panel\n\n");
 }
 
 void splitString(char string[]){
@@ -387,29 +435,26 @@ void assignSizeLR(node *currentPtr){
     }
 }
 
-void assignTopology(node *currentPtr){
-    if (currentPtr){
-        assignTopology(currentPtr->left);
-        currentPtr->topology_num = getDifTopology(traceNode(root,currentPtr->data)->total_size-1);
-        assignTopology(currentPtr->right);
-    }
-}
-
 void assignOrd(node *tree){
     int index1,index2;
-    int nodes[] = {50,30,20,40,70,80,60};
     int maxDepth = getMaxDepth(root);
 
     for (index1=maxDepth; index1>0; index1--)
         for(index2=0 ; index2<tree->total_size; index2++){
-            node * temp = traceNode(tree,nodes[index2]);
-            if (temp->left == NULL && temp->right == NULL)
-                temp->ord = 1;
+            node * temp = traceNode(tree,numArray[index2]);
+            if(temp->depth == index1){
+                if (temp->left == NULL && temp->right == NULL)
+                    temp->ord = 1;
 
-            else if (temp->left && temp->right){
-                temp->ord = temp->left->ord * temp->right->ord * ((factorial(temp->left_size + temp->right_size)) / (factorial(temp->left_size)*factorial(temp->right_size)));
+                else if (temp->left && temp->right){
+                    unsigned long long int fact_value = factorial(temp->left_size + temp->right_size) / (factorial(temp->left_size)*factorial(temp->right_size));
+                    unsigned long long int temp_val= temp->left->ord * temp->right->ord * fact_value;
+                    temp->ord = temp_val;
+                }
+                else if (temp->left || temp->right)
+                    temp->ord = 1;
             }
-            else if (temp->left || temp->right)
-                temp->ord = temp->topology_num;
+            else
+                continue;
         }
 }
